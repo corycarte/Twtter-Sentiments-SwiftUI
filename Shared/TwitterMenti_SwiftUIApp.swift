954f288cbaeafb663bceca540783dda4f3961af9
Cory Carte <7457451+corycarte@users.noreply.github.com>
2022-09-10 10:33:05 -0400

    Initial Commit

---
+++ Shared/TwitterMenti_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterMenti_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Cory Carte on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterMenti_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
